084f4e56c9d60ce4672730a2234e07c67912169e
Jaroslav Reken <jaroslav.reken@joyfulcraftsmen.com>
2019-02-28 16:31:12 +0000

    fix test for play

--- tests/Unit.Tests.ps1
+++ tests/Unit.Tests.ps1
@@ -1,7 +1,9 @@
+. .\functions\play.ps1
+
 Describe "Testing Get-It function" {
     Context "Input" {
         It "There should be an object output"{
-            (Get-Command Get-It).Parameters['aaa'].message | Should -Equal "aaa"
+            (Get-Command Get-It).Parameters['aaa'] | Should -BeNullOrEmpty
         }
     }
 }
