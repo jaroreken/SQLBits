419dc71b0f01d283f6aff2e20ef1ab6381af8b69
Jaroslav Reken <jaroslav.reken@joyfulcraftsmen.com>
2019-02-28 12:09:11 +0000

    test message

---
+++ functions/play.ps1
@@ -0,0 +1,23 @@
+<#
+.SYNOPSIS
+Short description
+
+.DESCRIPTION
+Long description
+
+.PARAMETER something
+Parameter description
+
+.EXAMPLE
+An example
+
+.NOTES
+General notes
+#>
+
+function Get-It {
+    param (
+        [string]$something
+    )
+    
+}
